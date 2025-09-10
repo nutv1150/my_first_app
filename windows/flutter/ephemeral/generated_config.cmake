fefa5f0dd278797205fbc9c1193e8275e471ddea
nut_y5 <165195341+nutv1150@users.noreply.github.com>
2025-09-10 20:50:01 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\VICTUS\\Downloads\\flutter_windows_3.32.6-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\moblie AJ.m\\my_first_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\VICTUS\\Downloads\\flutter_windows_3.32.6-stable\\flutter"
+  "PROJECT_DIR=C:\\moblie AJ.m\\my_first_app"
+  "FLUTTER_ROOT=C:\\Users\\VICTUS\\Downloads\\flutter_windows_3.32.6-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\moblie AJ.m\\my_first_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\moblie AJ.m\\my_first_app"
+  "FLUTTER_TARGET=C:\\moblie AJ.m\\my_first_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuNg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDc3YjRhNGNlMQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzJmMmIxOGJiMA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\moblie AJ.m\\my_first_app\\.dart_tool\\package_config.json"
+)
